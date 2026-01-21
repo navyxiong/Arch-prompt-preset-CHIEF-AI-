d524ef94102c5eda89be18999c5dc60649d0acbb
CHIEF AI <19092813@qq.com>
2026-01-21 15:15:39 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# Archi-prompt-preset
-from .Archi-prompt-preset import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+# archi_prompt_preset
+from .archi_prompt_preset import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
